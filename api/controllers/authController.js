97bc4eedfc4f1d12d0dd13d5ac98c02f15aaea64
Berko Pasha <berk579.32@gmail.com>
2024-03-03 21:05:20 +0300

    authController login completed

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs";
+import {createError} from "../utils/error.js"
 
 export const register = async (req, res, next)=>{
     try{
@@ -14,7 +15,22 @@ export const register = async (req, res, next)=>{
         await newUser.save()
         res.status(200).send("user has been created.")
     } catch(err){
-        res.send("yarra yedik")
+        next(err)
+    }
+}
+
+export const login = async (req, res, next)=>{
+    try{
+        const user = await User.findOne({username:req.body.username})
+        if(!user) return next(createError(404, "User not found(ben yazdim)"))
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if(!isPasswordCorrect) return next(createError(400, "wrong password or username(ben yazdim)"));
+
+        const {password, isAdmin, createdAt, updatedAt, __v} = user
+        
+        res.status(200).json(user)
+    } catch(err){
         next(err)
     }
 }
