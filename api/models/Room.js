59257066ea85167ecc618a91440ebab89da4bdd1
Berko Pasha <berk579.32@gmail.com>
2024-03-07 21:37:23 +0300

    room model has created

--- api/models/Room.js
+++ api/models/Room.js
@@ -1,56 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const RoomSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true //because we cannot create any hotel without a name
-    },
-    type:{
-        type: String,
-        required: true //because we cannot create any hotel without a name
-    },
-    city:{
-        type: String,
-        required: true //because we cannot create any hotel without a name
-    },
-    address:{
-        type: String,
-        required: true //because we cannot create any hotel without a name
-    },
-    distance:{
-        type: String,
-        required: true //because we cannot create any hotel without a name
-    },
-    photos:{
-        type: [String],
-        required: true //because we cannot create any hotel without a name
-    },
     title:{
         type: String,
-        required: true //because we cannot create any hotel without a name
+        required: true, 
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    maxPeople:{
+        type: Number,
+        required: true,
     },
     description:{
         type: String,
-        required: true //because we cannot create any hotel without a name
+        required: true 
     },
-    rating:{
-        type: String,
-        min:0,
-        max:5 //because we cannot create any hotel without a name
-    },
-    rooms:{
-        type: [String],//because it is gonna include room ids (the reason of [])
-    },
-    cheapestPrice:{
-        type: Number,
-        required: true
-    },
-    featured:{
-        type: Boolean,
-        default: false,  
-    },
-});
+    roomNumbers:[{number: Number, unavailableDates: [{type: Date}] }],
+},   
+    {timestamps: true} 
+);
 
 export default mongoose.model("Room", RoomSchema)
-
-//DEVAM EDICEM
